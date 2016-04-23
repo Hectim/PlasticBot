3a8abf695c07329b23aa6415ad02afbff6c5ca14
Brady Killeen <bradykilleen@gmail.com>
2016-04-22 18:10:21 -0600

    refactored some of the code, work in progress atm

--- README.md
+++ README.md
@@ -11,13 +11,14 @@ Twitch bot with ability to tell jokes and look up Runescape stats and ge prices.
 |!ge name/id|!ge raw rocktail|looks up the Runescape ge price of the item (work in progress)|
 
 # Installation
+##### [npm](https://www.npmjs.com/) must be installed
 * Download zip or clone repo.
-* Install npm and run "npm install" from the command line
+* Run "npm install" from the command line
 * Edit config.js with your RSN/Twitch
 * Create account.js file (see below)
-* Run "node bot.js" from the command line
+* Run "npm start" from the command line
 
-Twitch bots require Twitch accounts. Go to Twitch, make an account, and get the oauth key. Then create this file account.js in the same directory as the other files. It should look like this (but with your own info):
+Twitch bots require Twitch accounts. Go to Twitch, make an account, and get the oauth key. Then create a file called account.js in the same directory as the other files. It should look like this (but with your own info):
 ```javascript
 var account = {
   username: "TwitchBotAccount",
--- bot.js
+++ bot.js
@@ -1,43 +1,44 @@
 'use strict';
 
-let irc = require('tmi.js')
-let rsapi = require('runescape-api')
-let _ = require('lodash')
-let Promise = require('bluebird')
-let Humanize = require('humanize-plus')
-let jokes = require('./jokes.js')
-let ge = require('./ge.js')
-let config = require('./config.js').config
+let _       = require('lodash')
+let config  = require('./config.js').config
+let ge      = require('./ge.js')
+let irc     = require('tmi.js')
+let jokes   = require('./jokes.js')
 let options = require('./config.js').options
+let rsapi   = require('runescape-api')
+
+let Promise   = require('bluebird')
+let Humanize  = require('humanize-plus')
 
 let skillList = {
-  overall: ['overall', 'oa'],
-  attack: ['attack', 'att'],
-  defence: ['defence', 'defense', 'def'],
-  strength: ['strength', 'str'],
-  hitpoints: ['constitution', 'hp', 'hitpoints'],
-  ranged: ['ranged', 'range'],
-  prayer: ['prayer', 'pray'],
-  magic: ['magic', 'mage'],
-  cooking: ['cooking', 'coking', 'cook', 'cok'],
-  woodcutting: ['woodcutting', 'wc', 'woodchop'],
-  fletching: ['fletching', 'fletch'],
-  fishing: ['fishing', 'fish'],
-  firemaking: ['firemaking', 'fm'],
-  crafting: ['crafting', 'craft'],
-  smithing: ['smithing', 'smith', 'best'],
-  mining: ['mining', 'mine'],
-  herblore: ['herblore', 'herb', 'herby'],
-  agility: ['agility', 'agil'],
-  thieving: ['thieving', 'thiev', 'thief', 'thiefing', 'theiving', 'theiv', 'theif', 'theifing'],
-  slayer: ['slayer', 'slay'],
-  farming: ['farming', 'farm'],
-  runecrafting: ['runecrafting', 'rc'],
-  hunter: ['hunter', 'hunt', 'hunting'],
-  construction: ['construction', 'const', 'constr', 'construct'],
-  summoning: ['summoning', 'summon', 'sum', 'summ'],
-  dungeoneering: ['dungeoneering', 'dungeon', 'dung', 'dg', 'loldg'],
-  divination: ['divination', 'div', 'divi', 'divine']
+  overall:        ['overall', 'oa'],
+  attack:         ['attack', 'att'],
+  defence:        ['defence', 'defense', 'def'],
+  strength:       ['strength', 'str'],
+  hitpoints:      ['constitution', 'hp', 'hitpoints'],
+  ranged:         ['ranged', 'range'],
+  prayer:         ['prayer', 'pray'],
+  magic:          ['magic', 'mage'],
+  cooking:        ['cooking', 'coking', 'cook', 'cok'],
+  woodcutting:    ['woodcutting', 'wc', 'woodchop'],
+  fletching:      ['fletching', 'fletch'],
+  fishing:        ['fishing', 'fish'],
+  firemaking:     ['firemaking', 'fm'],
+  crafting:       ['crafting', 'craft'],
+  smithing:       ['smithing', 'smith', 'best'],
+  mining:         ['mining', 'mine'],
+  herblore:       ['herblore', 'herb', 'herby'],
+  agility:        ['agility', 'agil'],
+  thieving:       ['thieving', 'thiev', 'thief', 'thiefing', 'theiving', 'theiv', 'theif', 'theifing'],
+  slayer:         ['slayer', 'slay'],
+  farming:        ['farming', 'farm'],
+  runecrafting:   ['runecrafting', 'rc'],
+  hunter:         ['hunter', 'hunt', 'hunting'],
+  construction:   ['construction', 'const', 'constr', 'construct'],
+  summoning:      ['summoning', 'summon', 'sum', 'summ'],
+  dungeoneering:  ['dungeoneering', 'dungeon', 'dung', 'dg', 'loldg'],
+  divination:     ['divination', 'div', 'divi', 'divine']
 }
 
 function randomJoke(){
@@ -65,6 +66,37 @@ let client = new irc.client(options);
 client.on('chat', function(channel, user, message, self){
   message = message.toLowerCase()
   switch(true){
+    case /!commands/.test(message):
+      client.say(channel, "Supported commands: !joke, !help, !skill <name>, !ge <name> or <id> (work in progress)")
+      break;
+    case /!ge ([0-9]* | [a-z\ ]*)*/.test(message):
+      let thing = message.split(" ").slice(1).join("")
+      let parsedInt = parseInt(thing, 10)
+      if(Number.isInteger(parsedInt)){
+        thing = parsedInt
+      } else {
+        thing = ge[simplifyString(thing)]
+      }
+      console.log('req', thing)
+      rsapi.rs.ge.itemInformation(thing).then( (result) => {
+        let resultString = result.item.name + " ---- " + result.item.current.price + " gp [" + result.item.today.price.split(" ").join("") + " gp]"
+        Promise.resolve(resultString).then( (res) => {
+          client.say(channel, resultString)
+        })
+      })
+      break;
+    case /!help/.test(message):
+      client.say(channel, "You're now breathing manually.   You're now blinking manually.   You're now aware of your tongue.")
+      break;
+    case /!joke/.test(message):
+      client.say(channel, randomJoke())
+      break;
+    //case /!poll [0-9a-z\ ]*/.test(message):
+    //  let args = _.map(message.slice(6).match(/\w+|"[^"]+"/g), function(arg){ return arg.replace(/\"/g, '')})
+    //  if(args.length == 1){
+    //    args.push('Yes.')
+    //    args.push('No.')
+    //  }
     case /!skill [a-z]*/.test(message):
       let skillName = findAlias(message.split(" ")[1])
       //check against list of skill names
@@ -78,30 +110,6 @@ client.on('chat', function(channel, user, message, self){
           client.say(channel, resultString)
         })
       })
-      break;
-    case /!ge ([0-9]* | [a-z\ ]*)*/.test(message):
-      let thing = message.split(" ").slice(1).join("")
-      let parsedInt = parseInt(thing, 10)
-      if(Number.isInteger(parsedInt)){
-        thing = parsedInt
-      } else {
-        thing = ge[simplifyString(thing)]
-      }
-      rsapi.rs.ge.itemInformation(thing).then( (result) => {
-        let resultString = result.item.name + " ---- " + result.item.current.price + " gp [" + result.item.today.price.split(" ").join("") + " gp]"
-        Promise.resolve(resultString).then( (res) => {
-          client.say(channel, resultString)
-        })
-      })
-      break;
-    case /!joke/.test(message):
-      client.say(channel, randomJoke())
-      break;
-    case /!help/.test(message):
-      client.say(channel, "You're now breathing manually.   You're now blinking manually.   You're now aware of your tongue.")
-      break;
-    case /!commands/.test(message):
-      client.say(channel, "Supported commands: !joke, !help, !skill <name>, !ge <name> or <id> (work in progress)")
       break;
     case /!who/.test(message):
       client.say(channel, "I'm watching stats for: "+config.username)
